a98471e0c83f62933b6cef99e1c404814b110cff
mehakjit <mehakjitsingh@gmail.com>
2020-11-29 22:48:15 +0530

    [Mehakjit] Added: Demo React File

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import App2 from './App2'
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+  url='http://google.com/'
+  constructor(){
+    super()
+    this.state = {
+      userName: '',
+      title: "Hello React - 006"
+    }
+  }
+  onClick = ($event)=>{
+    console.log("save button is clicked: ", $event);
+    window.open(this.url,"_blank")
+  }
+  onNameChange=(event)=>{
+    console.log("Value is",event.target.value)
+    this.setState({userName: event.target.value})
+  }
+  render (){
+    return <div><h1>{this.state.title}</h1>
+    <h1>Hello &rarr; {this.state.userName} &larr; user dynamically changes</h1>
+    <img src = {logo} onClick={this.onClick} width="100px"></img>
+      <App2></App2>
+      <div className="text-box">
+          <input onChange={this.onNameChange}/>
+      </div>
     </div>
-  );
+  }
 }
 
 export default App;
